7e605b8ef4c256a5e4aaa92da3f1d17eca48cbfe
Julien Pivotto <roidelapluie@inuits.eu>
2013-05-26 11:44:00 +0200

    create SVG from templates

---
+++ hydesvg/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='hydesvg',
+      version='0.1',
+      description='RMLL extensions for Hyde and Jinja2',
+      author='Julien Pivotto',
+      #long_description='',
+      author_email='roidelapluie@gmail.com',
+      url='https://github.com/RMLL/rmll2013-website',
+      install_requires=[],
+      packages=[],
+      license= 'GPLv3+',
+      scripts=[],
+      py_modules=['hydesvg'],
+      keywords='hyde',
+     )
